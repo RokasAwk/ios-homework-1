b0321564204546cf13b0db34091745887b23d6b0
Rokas <icestar6215@gmail.com>
2024-10-24 22:07:29 +0800

    feat: add home page ui

--- ios homework 1/ContentView.swift
+++ ios homework 1/ContentView.swift
@@ -1,21 +1,68 @@
-//
-//  ContentView.swift
-//  ios homework 1
-//
-//  Created by 李翗祤 on 2024/10/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack{
+                
+                Text("Instant Developer")
+                    .font(.system(size: 40))
+                    .foregroundStyle(.white)
+                
+                Text("Get help form exports in 15 minutes") .foregroundStyle(.white)
+                
+                HStack{
+                    Image("student")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    Image("tutor")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+                .padding(.horizontal, 40)
+                .padding(.vertical, 15)
+                
+                Text("Need help with coding problems? Register!")
+                    .foregroundStyle(.white)
+                
+            }
+            
+            Spacer()
+            
+            VStack{
+                Button{
+                    // sign up onPressed
+                }label:{
+                    Text("Sign Up")
+                }
+                .frame(width: 200)
+                .padding()
+                .foregroundStyle(.white)
+                .background(.indigo)
+                .clipShape(RoundedRectangle(cornerRadius: 10)
+                )
+                
+                Button{
+                    // log in onPressed
+                }label:{
+                    Text("Log In")
+                }
+                .frame(width: 200)
+                .padding()
+                .foregroundStyle(.white)
+                .background(.gray)
+                .clipShape(RoundedRectangle(cornerRadius: 10)
+                )
+            }
+            
         }
-        .padding()
+        .background(
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+        )
     }
 }
 
