073c3e19e5dd08e99b970a51565fee3d26929f80
Rokas <icestar6215@gmail.com>
2024-10-24 22:12:56 +0800

    refactor: extract subview from widget

--- ios homework 1/ContentView.swift
+++ ios homework 1/ContentView.swift
@@ -12,16 +12,7 @@ struct ContentView: View {
                 
                 Text("Get help form exports in 15 minutes") .foregroundStyle(.white)
                 
-                HStack{
-                    Image("student")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                    Image("tutor")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                }
-                .padding(.horizontal, 40)
-                .padding(.vertical, 15)
+                BuildImageView()
                 
                 Text("Need help with coding problems? Register!")
                     .foregroundStyle(.white)
@@ -30,31 +21,7 @@ struct ContentView: View {
             
             Spacer()
             
-            VStack{
-                Button{
-                    // sign up onPressed
-                }label:{
-                    Text("Sign Up")
-                }
-                .frame(width: 200)
-                .padding()
-                .foregroundStyle(.white)
-                .background(.indigo)
-                .clipShape(RoundedRectangle(cornerRadius: 10)
-                )
-                
-                Button{
-                    // log in onPressed
-                }label:{
-                    Text("Log In")
-                }
-                .frame(width: 200)
-                .padding()
-                .foregroundStyle(.white)
-                .background(.gray)
-                .clipShape(RoundedRectangle(cornerRadius: 10)
-                )
-            }
+            BuildButton()
             
         }
         .background(
@@ -68,4 +35,49 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct BuildImageView: View {
+    var body: some View {
+        HStack{
+            Image("student")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Image("tutor")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        }
+        .padding(.horizontal, 40)
+        .padding(.vertical, 15)
+    }
+}
+
+struct BuildButton: View {
+    var body: some View {
+        VStack{
+            Button{
+                // sign up onPressed
+            }label:{
+                Text("Sign Up")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.indigo)
+            .clipShape(RoundedRectangle(cornerRadius: 10)
+            )
+            
+            Button{
+                // log in onPressed
+            }label:{
+                Text("Log In")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.gray)
+            .clipShape(RoundedRectangle(cornerRadius: 10)
+            )
+        }
+    }
 }
