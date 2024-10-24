20ec2cec11b8118a6cb096a7c8520f9016e595a8
Rokas <icestar6215@gmail.com>
2024-10-24 17:56:19 +0800

    Initial Commit

---
+++ ios homework 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios homework 1
+//
+//  Created by 李翗祤 on 2024/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios homework 1/ios_homework_1App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_homework_1App.swift
+//  ios homework 1
+//
+//  Created by 李翗祤 on 2024/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_homework_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
